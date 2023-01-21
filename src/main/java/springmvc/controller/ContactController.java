932fa1e6aabc6b5cd1c21dd5cf5a452a17512a96
meansh <anshdotcontact@gmail.com>
2023-01-21 17:52:03 +0530

    Extreme line of code reduced with the help of @ModelAttriBute in ContactController class

--- src/main/java/springmvc/controller/ContactController.java
+++ src/main/java/springmvc/controller/ContactController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,28 +20,36 @@ public class ContactController {
 	}
 
 	@RequestMapping(path = "/processform", method = RequestMethod.POST)
-	public String handleForm(@RequestParam("email") String userEmail, @RequestParam("userName") String userName,
-			@RequestParam("password") String userPassword, Model model) {
-
-		User user = new User();
-		user.setEmail(userEmail);
-		user.setUserName(userName);
-		user.setPassword(userPassword);
-
-		/*
-		 * System.out.println("Email " + userEmail); System.out.println("userName " +
-		 * userName); System.out.println("Password " + userPassword);
-		 */
+	public String handleForm(@ModelAttribute User user, Model model) {
 
 		System.out.println(user);
-		// process
-		/*
-		 * model.addAttribute("name", userName); model.addAttribute("password",
-		 * userPassword); model.addAttribute("email", userEmail);
-		 */
-		model.addAttribute("user", user);
+
 		return "success";
 
 	}
 
+	/*
+	 * @RequestMapping(path = "/processform", method = RequestMethod.POST) public
+	 * String handleForm(@RequestParam("email") String
+	 * userEmail, @RequestParam("userName") String userName,
+	 * 
+	 * @RequestParam("password") String userPassword, Model model) {
+	 * 
+	 * User user = new User(); user.setEmail(userEmail); user.setUserName(userName);
+	 * user.setPassword(userPassword);
+	 * 
+	 * 
+	 * System.out.println("Email " + userEmail); System.out.println("userName " +
+	 * userName); System.out.println("Password " + userPassword);
+	 * 
+	 * 
+	 * System.out.println(user); // process
+	 * 
+	 * model.addAttribute("name", userName); model.addAttribute("password",
+	 * userPassword); model.addAttribute("email", userEmail);
+	 * 
+	 * model.addAttribute("user", user); return "success";
+	 * 
+	 * }
+	 */
 }
